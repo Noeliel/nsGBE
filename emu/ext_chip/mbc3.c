64d332764db9045445dcf73d51c3a712d087c44f
Noeliel <noelieldev@gmail.com>
2021-05-09 23:34:55 +0200

    Initial RTC implementation

--- emu/ext_chip/mbc3.c
+++ emu/ext_chip/mbc3.c
@@ -21,10 +21,34 @@
 // mbc3 (duh)
 
 #include <env.h>
+#include <sys/time.h>
+
+union RTC_DH {
+    struct __attribute__((packed)) {
+#ifdef __LITTLE_ENDIAN__
+        _Bool day_high  : 1;
+        byte unused     : 5;
+        _Bool halt      : 1;
+        _Bool day_carry : 1;
+#else
+        _Bool day_carry : 1;
+        _Bool halt      : 1;
+        byte unused     : 5;
+        _Bool day_high  : 1;
+#endif
+    };
+    byte b;
+} rtc_dh;
 
 extern _Bool ext_ram_enabled;
 
 _Bool rtc_reg_selected = 0;
+uint8_t selected_rtc_reg = 0x8;
+_Bool latching = 0;
+
+struct timeval tv;
+time_t initial_tv_seconds;
+time_t time_elapsed;
 
 uint16_t mbc3_interpret_write(uint16_t offset, byte data)
 {
@@ -69,21 +93,38 @@ uint16_t mbc3_interpret_write(uint16_t offset, byte data)
         else
         {
             rtc_reg_selected = 1;
-            data &= 0xC;
-            active_ext_ram_bank.w = data;
+            selected_rtc_reg = data;
         }
 
         return 0x100;
     }
 
-    if (offset >= 0x6000 && offset <= 0x7FFF)
+    if (offset >= 0x6000 && offset <= 0x7FFF && rtc_reg_selected)
+    {
+        if (data == 0 && !latching)
+            latching = 1;
+
+        if (data == 1 && latching)
+        {
+            latching = 0;
+            gettimeofday(&tv, NULL);
+            time_elapsed = tv.tv_sec - initial_tv_seconds;
+            rtc_dh.day_high = (((time_elapsed / 3600) / 24) > 0xFF ? 1 : 0);
+        }
+
         return 0x100;
+    }
 
     //if (offset >= 0xA000 && offset <= 0xBFFF && !ext_ram_enabled)
         //return 0x100;
 
     if (offset >= 0xA000 && offset <= 0xBFFF && rtc_reg_selected)
+    {
+        if (selected_rtc_reg == 0xC)
+            rtc_dh.b = data;
+
         return 0x100;
+    }
 
     return 0;
 }
@@ -94,7 +135,34 @@ uint16_t mbc3_interpret_read(uint16_t offset)
         //return 0x1FF;
 
     if (offset >= 0xA000 && offset <= 0xBFFF && rtc_reg_selected)
-        return 0x1FF;
+    {
+        switch (selected_rtc_reg)
+        {
+            case 0x8: // seconds
+                return 0x100 | (time_elapsed % 60);
+                break;
+
+            case 0x9: // minutes
+                return 0x100 | ((time_elapsed / 60) % 60);
+                break;
+
+            case 0xA: // hours
+                return 0x100 | ((time_elapsed / 3600) % 24);
+                break;
+
+            case 0xB: // days (low)
+                return 0x100 | ((time_elapsed / 86400) & 0xFF);
+                break;
+
+            case 0xC: // days (high), carry bit, halt flag
+                return 0x100 | rtc_dh.b;
+                break;
+
+            default:
+                return 0x100;
+                break;
+        }
+    }
 
     return 0;
 }
@@ -124,6 +192,10 @@ uint32_t mbc3_setup()
 
     if (battery_enabled)
         battery_load(ext_ram_banks, ext_ram_bank_count);
+
+    gettimeofday(&tv, NULL);
+    initial_tv_seconds = tv.tv_sec; // todo: load this from save file instead of setting it to the current time
+    time_elapsed = 0;
 
     return 0;
 }
